c80ab4ca604c4747aabe9c4da237f7a524cb3cea
Oluwaseun Akogun <42413239+ojslick@users.noreply.github.com>
2025-05-13 04:52:06 -0500

    Initial commit - ECommerceStore MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ECommerceStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
